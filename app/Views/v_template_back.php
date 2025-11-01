d75fa56f896975b564fe17bf5b50fc5d62a8b16e
makhdisubulahsalam26-arch <makhdisubulahsalam26@gmail.com>
2025-11-01 08:26:24 +0700

    main

--- app/Views/v_template_back.php
+++ app/Views/v_template_back.php
@@ -13,9 +13,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
  <!-- Font Awesome Icons -->
-<link rel="stylesheet" href="<?= base_url('back') ?>/plugins/fontawesome-free/css/all.min.css">
+<link rel="stylesheet" href="<?= base_url('back/assets/AdminLTE-3.2.0/plugins/fontawesome-free/css/all.min.css')?>">
 <!-- Theme style -->
-<link rel="stylesheet" href="<?= base_url('back') ?>/dist/css/adminlte.min.css">
+<link rel="stylesheet" href="<?= base_url('back/assets/AdminLTE-3.2.0/dist/css/adminlte.min.css')?>">
 
 </head>
 <body class="hold-transition sidebar-mini">
